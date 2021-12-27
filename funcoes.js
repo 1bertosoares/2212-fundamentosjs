60314e1f7e07bdd9d2dc05dad3a3aca3675c054c
SAUDECE\humberto.silva <humberto.silva@saude.ce.gov.br>
2021-12-27 13:40:32 -0300

    envio de aulas funcoes e parametros

---
+++ funcoes.js
@@ -0,0 +1,14 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1 - declara a funcao
+
+function imprimeTexto (texto) {
+    console.log(texto)
+}
+
+// 2 - executa a funcao (1 ou + vezes)
+
+imprimeTexto ("oi");
+imprimeTexto ("outro texto");
