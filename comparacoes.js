2f68ab7523410d0c01ccd64995e896d326f41259
SAUDECE\humberto.silva <humberto.silva@saude.ce.gov.br>
2021-12-27 10:27:53 -0300

    praticas de comparações, console, erros, e scripts

---
+++ comparacoes.js
@@ -0,0 +1,13 @@
+// == (comparação implicita)
+
+const numero = 5;
+const texto =  "5";
+
+//console.log(numero === texto)
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == compara valor
+// === compara valor e tipo de dado
