2e8f5eeb6b9a61734de17ff4697f3c4261bbcb44
SAUDECE\humberto.silva <humberto.silva@saude.ce.gov.br>
2021-12-27 14:37:37 -0300

    aulas de expressoes-function e arrow-function

---
+++ expressoes-funcao.js
@@ -0,0 +1,12 @@
+// declaracao de função
+
+// function minhaFuncao(para) {
+//     //bloco de código
+// }
+
+// minhaFuncao("param")
+
+//expressão de função
+
+const soma = function(num1, num2) {return num1 + num2}
+console.log(soma(1, 1))
