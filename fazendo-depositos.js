707409e3d79a4a0cae15788bb2cdd5bed4134b22
SAUDECE\humberto.silva <humberto.silva@saude.ce.gov.br>
2022-01-24 13:33:49 -0300

    novos exercicios

---
+++ fazendo-depositos.js
@@ -0,0 +1,18 @@
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "123543652266",
+    email: "andre@email.com",
+    fones: ["55987456", "5514785697"],
+    dependentes: [
+        {
+            nome: "Sara Silva",
+            parantesco: "filha",
+            dataNasc: "20/03/2011"},
+        {
+            nome: "Samia Maria",
+            parantesco: "Filha",
+            dataNasc: "04/01/2014"
+        }
+    ],
+}
